28ac721c945f6935b48cb1f8dced27e4d71d8fa4
mehmet4564 <mehmettae@gmail.com>
2026-02-18 18:10:47 +0300

    chooser test edildi çoklu dosya yükleme eklendi

--- backend/full_api/app/yolo_choser.py
+++ backend/full_api/app/yolo_choser.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from ultralytics import YOLO
+from typing import List
 import os,shutil,uuid
 
 router = APIRouter()
@@ -9,53 +10,95 @@ class YOLOChooser:
     def __init__(self):
         base_path = os.path.dirname(__file__)
 
-        self.chooser_model = YOLO(os.path.join(base_path, "models/chooser.pt"))
+        model_path = os.path.join(base_path, "models", "classification.onnx")
+        self.chooser_model = YOLO(model_path, task="classify")
 
      
-        self.models = {
-            "ankle": YOLO(os.path.join(base_path, "models/ankle.pt")),
-            "chest": YOLO(os.path.join(base_path, "models/chest.pt")),
-            "hand": YOLO(os.path.join(base_path, "models/hand.pt")),
+        self.models = {  # model yollarını hazırlamakm için yapılan  python sözlüğüdür (dict:anahtar veri yapısıdır anahtar str olmalı)
+            "spine":YOLO(os.path.join(base_path, "models/spine.pt")),
+            "elbow":YOLO(os.path.join(base_path,"models/elbow.pt"))
         }
 
-    def predict(self, image_path: str):
+    def predict(self, image_path: str): # model  tahmin yapısıdır
         
-        result = self.chooser_model(image_path)[0]
-        class_id = int(result.boxes.cls[0])
-        class_name = ultres.names[class_id]
+        result = self.chooser_model(image_path)[0] # yüklediğimiz image ın pathini modele gönderir ve yolo bize liste yapısı şeklinde dönüt yapar bu listeden dönen ilk result sonucunu seçer içinden result.name ile anahtar kısmını buçuruz
+        
+         
+        
+        probs = result.probs.data.tolist() #tüm olasılıklarıu listekler
+        names = result.names #class isimleri sözlüğü
 
-     
-        model = self.models.get(class_name)
-        if model is None:
-            raise HTTPException(status_code=400, detail=f"Bu tür için model yok: {class_name}")
+    
+        indexed_probs = list(enumerate(probs)) #olasılıkları indexleri ile birlikte listeler
+        sorted_probs = sorted(indexed_probs,key=lambda x: x[1],reverse=True) # result.props.top1 ile modelin en yüksek güvenle tahmin ettiği sınıfın id sini alırız bu id yi int e çeviririz çünkü id genellikle int olur
+#x bir tuppledır 1,0098 gibi x[0] indexi id yi x[1] ise olasılığı temsil eder sorted fonksiyonu ile bu listeyi olasılığa göre azalan sırada sıralarız
+        top5 =sorted_probs[:5]
 
-       
-        detection_result = model(image_path)
+        top5_results=[
+             {
+                  "class_id":idx,
+                  "class name": names[idx],
+                  "confidence": prob
+             }
+             for idx, prob in top5
+        ]
+        
+        # yolo bir liste döndürür list=result0,result1,result2 gibi
+        #result ise bir nesne yapısıdır
+
+        # result {
+        # boxes : modelin görselde bulduğu nesneler
+        # names :sınıf isimleri dicti tek sonuç değil yönlendirme için sınıf_ıd=anahtar kelimedir, 0:ancle 1:chest gibi
+        # orig_img  : orijinal görüntü
+        # path : image yolu
+        # tojson() : sonuç dict yapısı geri dönüş için uygun yapı 
+        #{
+        # "name": "fracture",
+        # "class": 1,
+        # "confidence": 0.92,
+        # "box": {
+        #   "x1": 120,
+        #   "y1": 80,
+        #   "x2": 340,
+        #   "y2": 260
+        # }
+        # plot
+        #       }
 
         return {
-            "type": class_name,
-            "detections": detection_result[0].tojson()
-        }
-    
+             "top5_predictions": top5_results
+               }
 
-                        
+                    
 
 chooser = YOLOChooser()
 
 @router.post("/detect/")
-async def detect_xray(file: UploadFile = File(...)):
-
-        ext = os.path.splitext(file.filename)[1]
-        
-        temp_filename=f"{uuid.uuid4()}{ext}"(file.file)
-        temp_path = os.path.join("temp",temp_filename)
+async def detect_xray(files: List[UploadFile] = File(...)):
 
         os.makedirs("temp",exist_ok=True)
+        results=[]
 
-        with open (temp_path,"wb") as buffer :
-            shutil.copyfileobj(file.file,buffer)
+        for file in files:
+             
+             #geçici dosya yolu oluştur
+             ext=os.path.splitext(file.filename)[1]
+             temp_filename = f"{uuid.uuid4()}{ext}"
+             temp_path=os.path.join("temp",temp_filename)
 
+             #diske kaydetme
+             with open(temp_path,"wb") as buffer :
+                  shutil.copyfileobj(file.file,buffer)
+             #AI pipeline çalıştır
 
-        result =chooser.predict(temp_path)
-        os.remove(temp_path)
-        return result 
+             result = chooser.predict(temp_path)
+
+             #dosya adı ile birlikte kaydet
+             results.append({
+                  "file":file.filename,
+                  "result":result
+             })
+        return {
+             "total_files":len(results),
+             "results":results
+        }
